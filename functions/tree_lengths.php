15d5bce5ae79119d45fdfccece19bc6ad70fda40
unknown <75164519+dermestid@users.noreply.github.com>
2020-12-01 16:16:10 +0000

    Branch lengths correctly counted Fixes #25

--- functions/tree_lengths.php
+++ functions/tree_lengths.php
@@ -4,7 +4,7 @@ function tree_lengths($tree_filename) {
 	$tree = file_get_contents($tree_filename);
 
 	$TREE_REGEX = "/\s*tree\s+'[^']*'\s*=[^;]*;/i";
-	$BRANCH_LENGTH_REGEX = '/:\d+[\.,]?\d*/';
+	$BRANCH_LENGTH_REGEX = '/:[-e\.\d]+[,);]/';
 
 	// Find Newick trees within file
 	if(!preg_match_all($TREE_REGEX, $tree, $tree_lines)) {
@@ -12,7 +12,7 @@ function tree_lengths($tree_filename) {
 		return array();
 	}
 
-	// Get branch lengths array e.g. ((":0.0938", ":0.0013")) etc and sum them
+	// Get branch lengths array e.g. ((":0.093898,", ":0.001343)")) etc and sum them
 	$tree_lengths = array();
 
 	foreach ($tree_lines[0] as $tree_line)
@@ -26,7 +26,9 @@ function tree_lengths($tree_filename) {
 		$sum = 0;
 		foreach ($branch_lengths[0] as $len)
 		{
-			$sum += floatval(substr($len, 1)); // first char is ':'
+			// Note that NJ can produce negative branch lengths.
+			// We only need the total tree length, which is preserved if we take the absolute value.
+			$sum += abs(floatval(substr($len, 2, -1))); // starts with ':' and ends with ',' ')' or ';'
 		}
 		$tree_name = explode("'", $tree_line)[1];
 		$tree_lengths[$tree_name] = $sum;
