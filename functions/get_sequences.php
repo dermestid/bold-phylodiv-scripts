729ff5dac780fba3fd0e0eaf4c11817b3f107441
unknown <75164519+dermestid@users.noreply.github.com>
2020-12-03 15:47:50 +0000

    Skip broken entries in BOLD output Fixes #45

--- functions/get_sequences.php
+++ functions/get_sequences.php
@@ -53,8 +53,14 @@ function get_sequences($taxon, $marker) {
     // Step through stream line by line
     while ($entry = fgetcsv($source_handle, 0, $SOURCE_DELIMITER))
     {
-        
+        // Check for broken entry
+        $entry_size = count($entry);
+        if ($entry_size > $header_size) { 
+            // echo("Bad entry with size {$entry_size}".php_EOL);
+            continue;
+        }
         // make associative entry
+        $entry = array_pad($entry, $header_size, '');
         $entry = array_combine($header, $entry);
 
         // Check marker is the one we want to use
