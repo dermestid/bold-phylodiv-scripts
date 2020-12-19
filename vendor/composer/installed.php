f2dc379ba920a7b560fa87fd03c4075257bec77d
dermestid <75164519+dermestid@users.noreply.github.com>
2020-12-19 03:22:58 +0000

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0f7330ee4ef8c1d66007a5b18b323c1ddb3076f7',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f7330ee4ef8c1d66007a5b18b323c1ddb3076f7',
+    ),
+  ),
+);
